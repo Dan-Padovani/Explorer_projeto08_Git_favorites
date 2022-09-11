6f4478f591d9314e5abcfe62f47df96f1dc38884
Dan-Padovani <danpadovani84@gmail.com>
2022-09-11 19:39:31 -0300

    removing html trs for base building

--- app/js/Favorites.js
+++ app/js/Favorites.js
@@ -1,22 +1,4 @@
-// Consulta user github
-class GithubUser {
-	static search(username) {
-		const endpoint = `https://api.github.com/users/${username}`
-
-		return fetch(endpoint)
-		.then(data => data.json())
-		.then(data => {
-			const { login, name, public_repos, followers } = data
-
-			return {
-				login,
-				name,
-				public_repos,
-				followers
-			}
-		})
-	}
-}
+import { GithubUser } from "./GithubUSer.js"
 
 // Construçao dos dados da aplicação
 class Favorites {
---
+++ app/js/GithubUser.js
@@ -0,0 +1,19 @@
+// Consulta user github
+export class GithubUser {
+	static search(username) {
+		const endpoint = `https://api.github.com/users/${username}`
+
+		return fetch(endpoint)
+		.then(data => data.json())
+		.then(data => {
+			const { login, name, public_repos, followers } = data
+
+			return {
+				login,
+				name,
+				public_repos,
+				followers
+			}
+		})
+	}
+}
